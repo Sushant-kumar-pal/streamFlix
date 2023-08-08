3518cf9cab49ee66073d8586587d8ef7fb1604c2
Sushant-kumar-pal <palsushant2020@gmail.com>
2023-08-08 23:15:27 +0530

    StreamTube

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '0bc9610e34msh68596c00313a816p14aa73jsn3e4640eb9d1a',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
